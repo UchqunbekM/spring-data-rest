8bf83d695fc5fa333269191bdb589de4b197889d
UchqunbekM <akin-151@mail.ru>
2022-10-17 23:41:18 +0500

    Data-rest-web

--- src/main/java/com/example/appdatarest/Repository/AddressRepo/AddressRepo.java
+++ src/main/java/com/example/appdatarest/Repository/AddressRepo/AddressRepo.java
@@ -2,6 +2,8 @@ package com.example.appdatarest.Repository.AddressRepo;
 
 import com.example.appdatarest.Entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "address")
 public interface AddressRepo extends JpaRepository<Address, Integer> {
 }
